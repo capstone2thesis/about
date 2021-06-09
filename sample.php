5af0e0d75947855bee08c7fd1d62edf5591d92ab
capstone2thesis <84458147+capstone2thesis@users.noreply.github.com>
2021-06-09 21:30:28 +0800

    Add files via upload

---
+++ sample.php
@@ -0,0 +1,211 @@
+
+
+<style>
+    
+    body 
+{
+  Background-image: linear-gradient(rgba(0,0,0,0.1),rgba(0,0,0,0.1)),url(bacground.jpg);
+    background-size: cover;
+    background-position: center;
+  margin: 0;
+  font-family: courier;
+
+}
+
+
+section.contact-w3ls p.contact-agile1 {
+    font-size: 17px;
+	letter-spacing: 1px;
+    font-weight: normal;
+    padding-bottom: 10px;
+    line-height: 29px;
+    color: #fff;
+    text-align: center;
+    
+}
+section.contact-w3ls p.contact-agile1 strong {
+    font-family: 'Federo', sans-serif;
+    letter-spacing: 1.5px;
+    color: #ffce14!important;
+	font-size: 16px;
+    
+    
+}
+section.contact-w3ls p.contact-agile1 a {
+	color:#fff;
+    
+}
+section.contact-w3ls .contact-agileits,.contact-w3-agile1 {
+    background-color: rgba(0, 0, 0, 0.55);
+    padding: 20px 35px;
+}
+section.contact-w3ls h4 {
+	font-size:50px;
+	letter-spacing: 1.5px;
+	font-weight: normal;
+	color:#fff;
+	padding-bottom:20px;
+	font-family: 'Federo', bold;
+	text-align:center;
+}
+section.contact-w3ls p.contact-agile2 {
+	font-size:16px;
+	font-weight:normal;
+	padding-bottom:20px;
+	line-height:30px;
+	color:#ffce14;
+	text-align:center;
+}
+section.contact-w3ls label.contact-p1 {
+    font-size: 17px;
+	letter-spacing: 1px;
+    font-weight: 300;
+    color:#fff;
+    padding-bottom: 10px;
+	font-family: 'Federo', sans-serif;
+}
+.form-group {
+    margin-bottom: 5px;
+}
+section.contact-w3ls p.help-block {
+    color:#ff0500!important;
+}
+section.contact-w3ls input#name, section.contact-w3ls input#email, section.contact-w3ls input#phone  {
+	height:37px;
+}
+section.contact-w3ls input#name, section.contact-w3ls input#email, section.contact-w3ls input#phone {
+	font-size:17px;
+	font-weight:normal;
+	color:#fff;
+	background-color:transparent;
+	border-radius:0;
+	border-color:#fff;
+	font-family: 'Lato', sans-serif;
+}
+section.contact-w3ls button.btn.btn-primary {
+	color:#ffce14;
+    background-color:transparent;
+    border: 2px solid #ffce14;
+    font-size: 17px;
+    padding: 10px 20px;	
+	width:100%;
+	font-family: 'Federo', sans-serif;
+	margin: 25px 0 8px;
+}
+section.contact-w3ls button.btn.btn-primary:hover {
+	color: #fff;
+    background-color:#ffce14;
+    border: 2px solid #ffce14;
+}
+input::-webkit-input-placeholder {
+color: #fff !important;
+} 
+input:-moz-placeholder { 
+color: #fff !important;  
+} 
+input::-moz-placeholder {  
+color: #fff !important;  
+} 
+input:-ms-input-placeholder {  
+color: #fff !important;  
+}
+.footer-icons-agileinfo{
+	margin-left:0!important;
+}
+.contact-w3-agile1 iframe {
+    width: 100%;
+    height: 290px;
+    margin-top: 1.5em;
+    outline: none;
+    border: none;
+}
+    
+nav {
+        
+  background-color: 
+  padding-top: 5px;
+  padding-bottom: 5px;
+    }
+ul li {
+        display: inline-block;
+        padding: 20px 20px;
+        background-color: transparent;
+        margin: 0 10px;
+       
+    }
+    ul li:hover a {
+        color: whitesmoke;
+        font-size: 40px;
+        transition: all 0.1s ease;
+        
+    }
+    ul li a {
+        font-size: 45px;
+        color: gold;
+   
+    }
+ul {
+    display: flex;
+    justify-content: center;
+    } 
+    
+    
+    
+</style>
+
+<html>
+<head>
+    
+    <script src="https://kit.fontawesome.com/7770310cc2.js" crossorigin="anonymous"></script>
+    
+</head>
+    
+<body>
+    
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contact Us</h4>
+				<p class="contact-agile2">We'd Love To Hear From You!</p>
+				
+        <nav>
+             <ul>
+                    
+                    
+                    
+                    
+                <li><a href="http://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="http://www.twitter.com"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="http://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+        
+              
+             
+                    
+              </ul>
+        </nav>
+                 
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+	
+             <p class="contact-agile1"><strong>Telephone: </strong>8-7000</p>
+			<p class="contact-agile1"><strong>Phone: </strong>+9245680247</p>
+			<p class="contact-agile1"><strong>Email: </strong> <a href="mailto:name@example.com">INFO@JEVELYNCLOTHING.COM</a></p>
+			<p class="contact-agile1"><strong>Address: </strong> Pandacan, Manila, Philippines</p>
+																
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				
+                
+			</div>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61779.27390866552!2d120.96545107985656!3d14.587413221849104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9a97def49b1%3A0x769ee64bfe6191a!2sUNIQLO!5e0!3m2!1sen!2sin!4v1585585833046!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</div>
+		<div class="clearfix"></div>
+       
+        
+        
+	</div>
+</section>
+
+</body>
+</html>
